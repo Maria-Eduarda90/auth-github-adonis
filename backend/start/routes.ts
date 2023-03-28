62f1aa354bf204e35c4476d0ce483304d5893dc5
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-28 14:47:01 -0400

    added ;

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -2,11 +2,11 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/github/redirect', async ({ ally }) => {
   return ally.use('github').redirect()
-})
+});
 
 Route.get('/github/callback', async ({ request }) => {
   const code = request.qs().code;
   return code;
-})
+});
 
-Route.post('/authenticate', 'AuthenticateUsersController.handle')
+Route.post('/authenticate', 'AuthenticateUsersController.handle');
