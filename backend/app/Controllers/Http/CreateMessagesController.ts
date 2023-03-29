f6275a4f748bf88f0cf77feff09f2aa857e8b365
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-28 20:35:30 -0400

    taking my message and passing it to the routes file

--- backend/app/Controllers/Http/CreateMessagesController.ts
+++ backend/app/Controllers/Http/CreateMessagesController.ts
@@ -3,12 +3,12 @@ import CreateMessageService from 'App/Services/CreateMessageService'
 
 export default class CreateMessagesController {
   public async handle({ request }: HttpContextContract) {
-    const { message, user_id } = request.body()
+    const { message, user_id } = request.body();
 
-    const service = new CreateMessageService()
+    const service = new CreateMessageService();
 
-    const result = await service.execute(message, user_id)
+    const result = await service.execute(message, user_id);
 
-    return result
+    return result;
   }
 }
