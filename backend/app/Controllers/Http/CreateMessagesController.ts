f014f069209718488fe8b7cb5104754d6080fbda
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-28 20:32:49 -0400

    app/Controllers/Http/CreateMessagesController.ts

---
+++ backend/app/Controllers/Http/CreateMessagesController.ts
@@ -0,0 +1,14 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CreateMessageService from 'App/Services/CreateMessageService'
+
+export default class CreateMessagesController {
+  public async handle({ request }: HttpContextContract) {
+    const { message, user_id } = request.body()
+
+    const service = new CreateMessageService()
+
+    const result = await service.execute(message, user_id)
+
+    return result
+  }
+}
