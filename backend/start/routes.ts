83291f230e5a4c5ef9736dc20e92061f44eea62a
Mary Skywalker <snowdennaruto90@gmail.com>
2023-03-30 16:56:53 -0400

    leaving my message route protected

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -1,13 +1,14 @@
-import Route from '@ioc:Adonis/Core/Route'
+import Route from '@ioc:Adonis/Core/Route';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
-Route.get('/github/redirect', async ({ ally }) => {
-  return ally.use('github').redirect()
-});
+Route.get('/github/redirect', async ({ ally }: HttpContextContract) => {
+  return ally.use('github').redirect();
+})
 
-Route.get('/github/callback', async ({ request }) => {
+Route.get('/github/callback', async ({ request }: HttpContextContract) => {
   const code = request.qs().code;
   return code;
-});
+})
 
 Route.post('/authenticate', 'AuthenticateUsersController.handle');
 Route.post('/messages', 'CreateMessagesController.handle').middleware('auth');
